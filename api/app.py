e7a050349a855930efe728a36f5aac4d2b825066
vsmart-06 <srivishnuvusirikala@gmail.com>
2025-03-06 17:18:09 -0600

    Added a JavaScript wrapper for MistyGPT

---
+++ api/app.py
@@ -0,0 +1,73 @@
+from flask import Flask, request
+import openai
+
+import speech_recognition as sr 
+
+from dotenv import load_dotenv
+import io
+import os
+import json
+import base64
+
+load_dotenv()
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return '<marquee direction="right"><h1>The MistyGPT API is up and running!</h1></marquee>'
+
+def answer_prompt(prompt):
+    openai_client = openai.OpenAI(
+        api_key = os.getenv("OPENAI_API_KEY")
+    )
+
+    guides: dict = json.load(open("./api/guides.json"))["guides"]
+
+    found = False
+    for key in guides.keys():
+        if key in prompt:
+            message = guides[key].format(prompt = prompt)
+            found = True
+
+    if not found:
+        message = prompt
+
+    messages = [
+        {
+            "role": "user",
+            "content": f"You are the Misty 2 Robot and your primary purpose is to support as a socially assistive robot for people of all ages and abilities. Using this context, provide a response {'' if found else 'to '}'{message}'. You do not have to introduce yourself. If you do not understand the provided question, reply with 'Sorry, I didn't get that. Can you say that again?'. Unless the question is objective without requiring much elaboration, make your response at most 5 sentences long, but do not go below 3 sentences."
+        }
+    ]
+
+    response = openai_client.chat.completions.create(
+        model = "gpt-4o-mini",
+        messages = messages
+    )
+
+    return response.choices[0].message.content
+
+@app.route("/generate-response", methods = ["POST"])
+def generate_response():
+    rec = sr.Recognizer()
+
+    audio = request.get_json()
+    
+    file = io.BytesIO(base64.b64decode(audio["audio"]))
+
+    with sr.AudioFile(file) as audio:
+        data = rec.record(audio)
+
+    try:
+        msg = rec.recognize_google(data).lower()
+        print("Input:", msg)
+        output = answer_prompt(msg)
+        print("Output:", output)
+    except Exception as e:
+        print("Error:", e)
+        return {"error": str(e.with_traceback(e.__traceback__))}
+
+    return {"message": output}
+
+if __name__ == "__main__":
+    app.run(debug = True)
---
+++ src/MistyGPT.js
@@ -0,0 +1,47 @@
+function startListening() {
+    misty.RegisterEvent("KeyPhraseRecognized","KeyPhraseRecognized", 10, false);
+
+    misty.StartKeyPhraseRecognition();
+
+    misty.Debug("Started listening for key phrase");
+}
+
+function _KeyPhraseRecognized() {
+    misty.AddReturnProperty("VoiceRecord", "Filename");
+    misty.AddReturnProperty("VoiceRecord", "Success");
+    misty.AddReturnProperty("VoiceRecord", "ErrorCode");
+    misty.AddReturnProperty("VoiceRecord", "ErrorMessage");
+    misty.RegisterEvent("VoiceRecord", "VoiceRecord", 10, false);
+
+    misty.Debug("Started recording data");
+}
+
+function _VoiceRecord(data) {
+    var audioFileName = data.AdditionalResults[0];
+    var success = data.AdditionalResults[1];
+    var errorCode = data.AdditionalResults[2];
+    var errorMessage = data.AdditionalResults[3];
+
+    if (success) {
+        misty.Debug("Successfully captured speech to " + audioFileName);
+        misty.GetAudioFile(audioFileName, "sendAudio");
+    } else {
+       misty.Debug("Error: " + errorCode + ". " + errorMessage);
+    }
+}
+
+function sendAudio(data) {
+    var audio = data.Result.Base64
+
+    var url = "https://31b3-192-17-168-86.ngrok-free.app/generate-response";
+    misty.SendExternalRequest("POST", url, null, null, JSON.stringify({"audio": audio}), false, false, null, "application/json");
+}
+
+function _SendExternalRequest(data) {
+    var response = JSON.parse(data.Result.ResponseObject.Data);
+    misty.Speak(response.message, true);
+
+    startListening();
+}
+
+startListening();
